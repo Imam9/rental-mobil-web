61889e347f5f8906530687f9427c2e9831114cbb
imam9 <imamzunaidi4@gmail.com>
2020-08-18 23:40:51 +0700

    update template customer

--- application/controllers/customer/Dashboard.php
+++ application/controllers/customer/Dashboard.php
@@ -10,13 +10,13 @@ class Dashboard extends CI_controller{
         $this->load->view('templates_customer/footer');
     }
 
-    public function detail_mobil($id){
-        $data['detail'] = $this->rental_model->ambil_id_mobil($id);
-        $this->load->view('templates_customer/header');
-        $this->load->view('customer/detail_mobil',$data);
-        $this->load->view('templates_customer/footer');
+    // public function detail_mobil($id){
+    //     $data['detail'] = $this->rental_model->ambil_id_mobil($id);
+    //     $this->load->view('templates_customer/header');
+    //     $this->load->view('customer/detail_mobil',$data);
+    //     $this->load->view('templates_customer/footer');
 
-    }
+    // }
 }
 
 ?>
